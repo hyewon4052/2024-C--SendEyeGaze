4f79c843b437195c469bc41b6e5d9297c6a5ed2c
hyewon4052 <s2311@e-mirim.hs.kr>
2024-12-03 08:02:51 +0900

    결과 헤더파일

---
+++ SendEyeGaze/Result.h
@@ -0,0 +1,13 @@
+#ifndef RESULT_H
+#define RESULT_H
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+using namespace std;
+
+string getRandomFriend();
+
+int result(); // 반환 타입을 void로 수정
+
+#endif // RESULT_H
