8dd78ed31c9fd67fb945042071ae1c41487ad2f0
hyewon4052 <s2311@e-mirim.hs.kr>
2024-12-02 10:14:43 +0900

    헤더파일 추가

---
+++ SendEyeGaze/Game.h
@@ -0,0 +1,8 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Graphics.hpp>
+
+int runGame(); // 반환 타입을 int로 설정
+
+#endif // GAME_H
---
+++ SendEyeGaze/StartScreen.h
@@ -0,0 +1,8 @@
+#ifndef START_SCREEN_H
+#define START_SCREEN_H
+
+#include <SFML/Graphics.hpp>
+
+bool startGame();
+
+#endif // START_SCREEN_H
